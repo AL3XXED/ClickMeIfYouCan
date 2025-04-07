b7733dbcc967a809cd595645187bc45e4224a488
Hasenkrug <alexander.hasenkrug@training.lug-ag.de>
2025-04-07 14:49:00 +0200

    Probleme mit dem Button das Spiel zu Starten und ihn anschließend dem Mauszeiger ausweichen zu lassen.

--- ClickMe/ViewModels/SpielViewModel.cs
+++ ClickMe/ViewModels/SpielViewModel.cs
@@ -10,17 +10,17 @@ public class SpielViewModel : INotifyPropertyChanged
 {
     private string _spielerName;
     private int _punkte;
-    private Timer _spielTimer;
-    private Timer _sprungTimer;
-    private IDispatcherTimer _bewegungsTimer;
     private int _spielDauer = 60;
     private int _restlicheZeit;
     private int _schwierigkeit = 1;
     private double _buttonX;
     private double _buttonY;
-    private double _buttonSpeedX;
-    private double _buttonSpeedY;
     private bool _spielLauft;
+
+    private Timer _spielTimer;
+    private Timer _sprungTimer;
+
+    Random random = new Random();
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -117,10 +117,12 @@ public class SpielViewModel : INotifyPropertyChanged
     }
 
     public ICommand ButtonClickedCommand { get; }
+    public ICommand TapCommand { get; }
 
     public SpielViewModel()
     {
-        ButtonClickedCommand = new Command(OnButtonClicked);
+        TapCommand = new Command<Point>(HandleTap);
+        ButtonClickedCommand = new Command(ButtonClicked);
 
         _spielTimer = new Timer(1000);
         _spielTimer.Elapsed += (s, e) =>
@@ -133,91 +135,14 @@ public class SpielViewModel : INotifyPropertyChanged
             });
         };
 
-        _bewegungsTimer = Application.Current.Dispatcher.CreateTimer();
-        _bewegungsTimer.Interval = TimeSpan.FromMilliseconds(16);
-        _bewegungsTimer.Tick += BewegungsTimer_Tick;
-
-        _sprungTimer = new Timer();
-        _sprungTimer.Elapsed += (s, e) =>
+        _sprungTimer = new Timer
         {
-            MainThread.BeginInvokeOnMainThread(BewegeButton);
+            AutoReset = true
         };
+        _sprungTimer.Elapsed += (s, e) => BewegeButton();
     }
 
-    public void StartGame(string spielerName, int schwierigkeit)
-    {
-        SpielerName = spielerName;
-        Schwierigkeit = schwierigkeit;
-        Punkte = 0;
-        Zeit = SpielDauer;
-        _spielLauft = true;
-
-        double geschwindigkeit = Schwierigkeit switch
-        {
-            1 => 1,
-            2 => 2,
-            3 => 3,
-            _ => 1
-        };
-
-        var zufall = new Random();
-        _buttonSpeedX = geschwindigkeit * (zufall.NextDouble() < 0.7 ? zufall.Next(2, 4) : 1) * (zufall.Next(2) == 0 ? -1 : 1);
-        _buttonSpeedY = geschwindigkeit * (zufall.NextDouble() < 0.7 ? zufall.Next(2, 4) : 1) * (zufall.Next(2) == 0 ? -1 : 1);
-
-        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
-        ButtonX = (displayInfo.Width / displayInfo.Density - 100) / 2;
-        ButtonY = (displayInfo.Height / displayInfo.Density - 100) / 2;
-
-        _spielTimer.Start();
-        _bewegungsTimer.Start();
-
-        if (schwierigkeit >= 2)
-        {
-            _sprungTimer.Interval = schwierigkeit == 2 ? 500 : 250;
-            _sprungTimer.Start();
-        }
-    }
-
-    private void BewegungsTimer_Tick(object sender, EventArgs e)
-    {
-        if (_spielLauft) return;
-
-        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
-        double width = displayInfo.Width / displayInfo.Density;
-        double height = displayInfo.Height / displayInfo.Density;
-
-        double newX = ButtonX + _buttonSpeedX;
-        double newY = ButtonY + _buttonSpeedY;
-
-        if (newX <= 0 || newX >= width - 100)
-        {
-            _buttonSpeedX = -_buttonSpeedX;
-            newX = Math.Clamp(newX, 0, width - 100);
-        }
-
-        if (newY <= 0 || newY >= height - 100)
-        {
-            _buttonSpeedY = -_buttonSpeedY;
-            newY = Math.Clamp(newY, 0, height - 100);
-        }
-
-        ButtonX = newX;
-        ButtonY = newY;
-        AbsoluteLayout.SetLayoutBounds((Button)sender, new(ButtonX, ButtonY, 100, 100));
-    }
-
-    private void BewegeButton()
-    {
-        var random = new Random();
-        var width = Application.Current.MainPage.Width;
-        var height = Application.Current.MainPage.Height;
-
-
-        ButtonX = random.Next(0, (int)(width - 100));
-        ButtonY = random.Next(0, (int)(height - 100));
-    }
-
-    private void OnButtonClicked(object sender, EventArgs e)
+    public void ButtonClicked()
     {
         if (!_spielLauft)
         {
@@ -226,51 +151,75 @@ public class SpielViewModel : INotifyPropertyChanged
         else
         {
             Punkte++;
-            _buttonSpeedX = -_buttonSpeedX;
-            _buttonSpeedY = -_buttonSpeedY;
-
-            if (!_bewegungsTimer.IsRunning)
-                _bewegungsTimer.Start();
         }
+    }
+    public void StartGame(string spielerName, int schwierigkeit)
+    {
+        SpielerName = spielerName;
+        Schwierigkeit = schwierigkeit;
+        Punkte = 0;
+        Zeit = SpielDauer;
+        _spielLauft = true;
+
+        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+        var width = displayInfo.Width / displayInfo.Density;
+        var height = displayInfo.Height / displayInfo.Density;
+
+        ButtonX = (width - 100) / 2;
+        ButtonY = (height - 100) / 2;
+
+        _spielTimer.Start();
+
+        _sprungTimer.Interval = Schwierigkeit switch
+        {
+            1 => 1500,       
+            2 => 600,     
+            3 => 300,     
+            _ => 1000     
+        };
+        _sprungTimer.Start();
+        
+    }
+
+    private void BewegeButton()
+    {
+        if (!_spielLauft) return;
+
+        var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+        var width = displayInfo.Width / displayInfo.Density;
+        var height = displayInfo.Height / displayInfo.Density;
+
+        ButtonX = random.Next(20, (int)(width - 120));
+        ButtonY = random.Next(20, (int)(height - 120));
+    }
+
+    public void HandleTap(Point tapPosition)
+    {
+        if (!_spielLauft) return;
+
+        double strecke = Math.Sqrt(Math.Pow(tapPosition.X - ButtonX, 2) + Math.Pow(tapPosition.Y - ButtonY, 2));
+
+        if (strecke < 25)
+        {
+            BewegeButton();
+        }
+
     }
 
     private async void SpielEnde()
     {
         _spielLauft = false;
         _spielTimer.Stop();
-        _bewegungsTimer.Stop();
         _sprungTimer.Stop();
 
         await MainThread.InvokeOnMainThreadAsync(async () =>
         {
-            if (Application.Current?.MainPage != null)
-            {
-                await Application.Current.MainPage.DisplayAlert("Spiel beendet", $"Du hast {Punkte} Punkte erreicht!", "OK");
-                await Application.Current.MainPage.Navigation.PopAsync();
-            }
+            await Application.Current.MainPage.DisplayAlert("Spiel beendet", $"Du hast {Punkte} Punkte erreicht!", "OK");
+            await Application.Current.MainPage.Navigation.PopAsync();
         });
     }
 
-    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
-    {
-        var button = sender as Button;
-        var position = e.GetPosition((Button)sender);
 
-        if (position.HasValue)
-        {
-            double centerX = button.Width / 2;
-            double centerY = button.Height / 2;
-            if (position.Value.X < centerX)
-                _buttonSpeedX = -Math.Abs(_buttonSpeedX);
-            else
-                _buttonSpeedX = Math.Abs(_buttonSpeedX);
-
-            if (position.Value.Y < centerY)
-                _buttonSpeedY = -Math.Abs(_buttonSpeedY);
-            else
-                _buttonSpeedY = Math.Abs(_buttonSpeedY);
-        }
-    }
 
     private void OnPropertyChanged(string propertyName) =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- ClickMe/Views/SpielPage.xaml.cs
+++ ClickMe/Views/SpielPage.xaml.cs
@@ -1,11 +1,28 @@
+using ClickMe.ViewModels;
+
 namespace ClickMe.Views;
 
 public partial class SpielPage : ContentPage
 {
-	public SpielPage()
-	{
-		InitializeComponent();
-	}
+    private SpielViewModel? ViewModel => BindingContext as SpielViewModel;
+    public SpielPage()
+    {
+        InitializeComponent();
+    }
 
+    private void Bewegung(object sender, TappedEventArgs e)
+    {
+        if (ViewModel == null) return;
 
+        var tapPosition = e.GetPosition((View)sender);
+        if (tapPosition.HasValue)
+        {
+            ViewModel.HandleTap(new Point(tapPosition.Value.X, tapPosition.Value.Y));
+        }
+    }
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        ViewModel?.ButtonClicked();
+    }
+    
 }
